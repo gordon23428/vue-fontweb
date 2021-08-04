713a5824a5f2c3b82360ad8a780265eeffae8244
gordon <pigsong23428@gmail.com>
2021-08-04 17:58:15 +0800

    新增動態牆

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,45 @@
 <template>
   <div class="home">
     <Navbar/>
-    <div class="banner">
-      <div class="banner-text">
-        <h1>想悠游在美麗的大海嗎</h1>
-        <p>歡迎加入小林潛水</p>
-        <button>立即加入</button>
+    <div class="slider-contaner">
+      <div class="slider">
+        <div class="slider-item slider-item1">
+          <div class="introduce mt-3">
+            <h2 class="text-white">馬爾地夫風暴魚</h2>
+            <p class="text-white">置身魚群風暴中</p>
+            <button class="btn btn-light">立即詢問</button>
+          </div>
+        </div>
+        <div class="slider-item slider-item2">
+          <div class="introduce mt-3">
+            <h2 class="text-white">沖繩藍洞</h2>
+            <p class="text-white">悠遊美麗的大海</p>
+            <button class="btn btn-light">立即詢問</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="container mt-5">
-    <h2 class="index">
-      您所想像的到的包包，都在小林賣包
-    </h2>
-    <div class="row">
-      <Sidebar/>
-      <div class="main col-9">
-        <form>
-          <div class="d-flex mb-3">
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="search">
-            <button type="submit" class="btn btn-primary">search</button>
-          </div>
-        </form>
-          <router-view/>
+  <router-view></router-view>
+  <div class="footer text-center bg-dark py-5">
+      <div class="text-white mb-3">個人作品非商業用途用</div>
+      <div class="icon">
+        <i class="fab fa-facebook fa-lg text-white mx-3"></i>
+        <i class="fab fa-line fa-lg text-white mx-3"></i>
+        <i class="fab fa-instagram fa-lg text-white mx-3"></i>
+        <i class="fas fa-phone-alt fa-lg text-white mx-3">0911-XXX-XXX</i>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
-import Sidebar from '@/components/Sidebar.vue'
+// import Sidebar from '@/components/Sidebar.vue'
 export default {
   name: 'Home',
   components: {
-    Navbar,
-    Sidebar
+    Navbar
   }
 }
 </script>
